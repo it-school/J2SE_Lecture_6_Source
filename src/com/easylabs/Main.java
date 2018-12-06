47c8ae7f4178c1928cdd315f0e37952ffc3c7889
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2018-12-06 17:45:54 +0200

    Update Main.java

--- src/com/easylabs/Main.java
+++ src/com/easylabs/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Student University[] = new Student[5];
 
-        String speciaity = "Программная инженерия";
+        String speciaity = "Разработка программного обеспечения";
 
         University[0] = new Student("Алексей", speciaity);
-        University[1] = new Student("Ваня", speciaity);
+        University[1] = new Student("Иван", speciaity);
         University[2] = new Student("Александр", speciaity);
         University[3] = new Student("Наталия", speciaity);
         University[4] = new Student("Виктор", speciaity);
